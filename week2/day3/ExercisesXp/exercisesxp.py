f934282f3d12fd13ddc36c51ff7ee0ae9b3f0123
jveiga17 <jveiga17@icloud.com>
2023-11-28 16:25:04 +0200

    day3 in progress

---
+++ week2/day3/ExercisesXp/exercisesxp.py
@@ -0,0 +1,13 @@
+#Exercise1
+
+keys = ['Ten', 'Twenty', 'Thirty'] #Given lists
+values = [10, 20, 30]
+
+result_dict = dict(zip(keys, values)) #Making the dictionary 
+
+print(result_dict) #Printing results
+
+
+
+
+
