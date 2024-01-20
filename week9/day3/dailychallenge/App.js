ce0b5d6d64d83ece22a9f8c2bb6dfd0bcb62a6cc
Joao Veiga <144750954+jveiga17@users.noreply.github.com>
2024-01-20 19:25:29 +0200

    Add files via upload

---
+++ week9/day3/dailychallenge/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const images = [
+  './img/hongkong.jpg',
+  './img/macao.webp',
+  './img/japan.webp',
+  './img/lasvegas.webp',
+];
+
+const cities = ['Hong Kong', 'Macao', 'Japan', 'Las Vegas'];
+
+function App() {
+  return (
+    <div className="carousel-container">
+      {/* carousel comp from the carousel package */}
+      <Carousel>  
+        {images.map((path, index) => (
+          <div key={index} className="carousel-item">
+            <img src={process.env.PUBLIC_URL + path} alt={`City ${index + 1}`} />
+            <div className="city-overlay">
+              <p>{cities[index]}</p>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
+
+export default App;
